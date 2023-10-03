5326b3be2473a8d81b2f45f79724fc4804cb5d7b
JayderHerreraMora <hjaider003@gmail.com>
2023-10-03 17:28:27 -0500

    Tarea Realizada

---
+++ script.js
@@ -0,0 +1,31 @@
+let listaDeContactos = ["raulGonzales", "jhonJimenez", "joseHernandez", "mariaRamirez"];
+let contactoNuevo;
+contactoNuevo = "JaiderHerrera"
+
+//Funcion para agregar nuevo contacto
+function nuevoContacto() {
+    listaDeContactos.unshift(contactoNuevo);
+
+    return listaDeContactos;
+}
+
+nuevoContacto();
+console.log(listaDeContactos);
+
+let nuevaLista;
+//Funcion para borrar un contacto en la lista
+function borrarContacto(){
+    nuevaLista = listaDeContactos.pop();
+
+    return listaDeContactos;
+}
+
+borrarContacto()
+console.log("Elemento Borrado: " + nuevaLista);
+
+//Funcion para imprimir en consola la lista actualizada
+function listaActualizada(){
+    console.log(listaDeContactos);
+}
+
+listaActualizada();
